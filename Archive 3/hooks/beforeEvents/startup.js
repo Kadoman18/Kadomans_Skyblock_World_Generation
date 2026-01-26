2601dab6761cb02410b24ddaa977c3dca8d45647
K-den <kbreinholt@icloud.com>
2026-01-26 14:58:19 -0500

    deleted archive folder

--- Archive 3/hooks/beforeEvents/startup.js
+++
@@ -1,7 +0,0 @@
-import { system } from "@minecraft/server";
-import { kadoVault } from "../../registry/vaultCustomComp";
-
-system.beforeEvents.startup.subscribe((eventData) => {
-	const { blockComponentRegistry, customCommandRegistry, itemComponentRegistry } = eventData;
-	blockComponentRegistry.registerCustomComponent("kado:vault", kadoVault);
-});
